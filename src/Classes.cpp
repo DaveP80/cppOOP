2a560738c99889a423d8b2b28285f836294102ae
davep80 <davidpaq1@gmail.com>
2023-03-16 05:43:10 +0000

    cmake setup

---
+++ src/Classes.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+#include "cat.h"
+
+using namespace std;
+
+int main()
+{
+    Cat cat;
+    cat.speak();
+    cat.run();
+    return 0;
+}
---
+++ src/cat.cpp
@@ -0,0 +1,12 @@
+#include "cat.h"
+#include <iostream>
+#include "cat.h"
+using namespace std;
+
+void Cat::speak(){
+    cout << "meoww" << endl;
+}
+
+void Cat::run(){
+    cout << "running fast" << endl;
+}
