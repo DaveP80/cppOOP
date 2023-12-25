6c78c36c0808a47762015bd20a360d4a9d833803
davep80 <davidpaq1@gmail.com>
2023-12-25 02:44:19 +0000

    solved leetcode no 1496

---
+++ cprogramming/pathcrossing.c
@@ -0,0 +1,45 @@
+// Leetcode No. 1496
+#include <stdio.h>
+#include <stdbool.h>
+bool isPathCrossing(char* path) {
+    int stor[strlen(path) + 1][2];
+    stor[0][0] = 0;
+    stor[0][1] = 0;
+    int idx = 1;
+
+    int length = sizeof(stor) / sizeof(stor[0]);  // Calculate the length outside the loop
+
+    for (int i = 0; i < strlen(path); i++) {
+        int temp[2];
+
+        if (path[i] == 'N') {
+            temp[0] = stor[idx-1][0];
+            temp[1] = stor[idx-1][1] + 1;
+        }
+        if (path[i] == 'S') {
+            temp[0] = stor[idx-1][0];
+            temp[1] = stor[idx-1][1] - 1;
+        }
+        if (path[i] == 'E') {
+            temp[0] = stor[idx-1][0] + 1;
+            temp[1] = stor[idx-1][1];
+        }
+        if (path[i] == 'W') {
+            temp[0] = stor[idx-1][0] - 1;
+            temp[1] = stor[idx-1][1];
+        }
+
+        // Check for intersections with all previous positions
+        for (int z = 0; z < idx; z++) {
+            if (temp[0] == stor[z][0] && temp[1] == stor[z][1]) {
+                return true;
+            }
+        }
+
+        stor[idx][0] = temp[0];
+        stor[idx][1] = temp[1];
+        idx++;
+    }
+
+    return false;
+}
