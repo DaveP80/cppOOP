7c31459bbd3dd5e20042e8b5a3ee15f9a5bf8ce8
davep80 <davidpaq1@gmail.com>
2024-01-08 05:12:26 +0000

    david paquette leetcode solution for No. 1207

---
+++ leet/python/uniqueoccurances.py
@@ -0,0 +1,15 @@
+# LeetCode No. 1207
+# Given an array of integers arr, return true if the number of occurrences of each value in 
+# the array is unique or false otherwise.
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        stor = {}
+        c = []
+        for n,m in enumerate(arr):
+            if m not in c:
+                temp = str(arr.count(m))
+                if temp in stor:
+                    return False
+                stor[temp] = m
+                c.append(m)
+        return True
